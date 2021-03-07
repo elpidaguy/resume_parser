b44ad3296872f75def1738509fe3382315499d21
elpidaguy <exabytes.js@gmail.com>
2021-03-07 14:49:13 +0530

    Upload Resume File Server Code Modification

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, redirect, request,Response, render_template, url_for, send_from_directory
+from flask import Flask, jsonify, redirect, request, Response, render_template, url_for, send_from_directory
 import base64
 import os
 import json
@@ -6,34 +6,81 @@ from flask_cors import CORS, cross_origin
 import hashlib
 import math
 import requests
+# import pyrebase
+from werkzeug.utils import secure_filename
+# from resume_parser2.resumeparse import resumeparse
+# from sseclient import SSEClient
 
-app = Flask(__name__)  #initializing flask app
-CORS(app) #to avoid CORS errors
+app = Flask(__name__)  # initializing flask app
+CORS(app)  # to avoid CORS errors
 
-@app.route('/UploadFile',methods=['POST'])
+UPLOAD_FOLDER = '../uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# cong = {
+
+#     "apiKey": "AIzaSyANPQjD8WE5I6IqUsGdghkgtls_ZcUM2hg",
+#     "authDomain": "myproject-99cc3.firebaseapp.com",
+#     "databaseURL": "https://myproject-99cc3-default-rtdb.firebaseio.com",
+#     "projectId": "myproject-99cc3",
+#     "storageBucket": "myproject-99cc3.appspot.com",
+#     "messagingSenderId": "686650502759",
+#     "appId": "1:686650502759:web:336484c7ccd1882f866ec2"
+# }
+
+# firebase = pyrebase.initialize_app(cong)
+# storage = firebase.storage()
+
+# path_on_cloud = "resumes/file.pdf"
+
+
+# def parse_file(filename):
+#     resume = resumeparse.read_file(filename)
+#     return resume
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET'])
+def Welcome():
+    return jsonify({'status': "success", 'message': "Welcome to Resume Parser API"})
+
+@app.route('/UploadFile', methods=['POST'])
 def UploadFile():
+    print("inside upload file")
+    if request.method == 'POST':
+        print(request)
+        print(request.files)
+        # check if the post request has the file part
+        if 'resume' not in request.files:
+            print('No file part')
+            return jsonify({'status': "failure", 'message': "No file part"})
+        file = request.files['resume']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return jsonify({'status': "failure", 'message': "No Selected File"})
+        if file and allowed_file(file.filename):
+            print(file.filename)
+            # filename = secure_filename(file.filename)
+            # path_on_cloud = "resumes/"+filename
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # storage.child(path_on_cloud).put(
+            #     os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # resume = parse_file(os.path.join(
+            #     app.config['UPLOAD_FOLDER'], filename))
+            # print(resume)
 
-    #Reference : https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
-
-    #NOTE: Try to open this project with VSCODE and install 1.python module 2. TODO Tree 3. Rainbow Brackets
-    #Follow these steps to complete your API
-
-    '''TODO: 
-
-    Step 0 : Run pip install -r requirements.txt
-
-    Step 1: Upload File via REQUESTS to local directory named upload (You can ask sir if its sufficient if we upload all files on server rather than firebase ? upload in upload folder only : go to step 4 )
-
-    STEP 2: Call functions from resumeparser.py to parse file and return the result generated to this function 
-    
-    STEP 3: Set the result in local variable and then call FIREBASE db API from Python to save the result in DB
-    
-    STEP 4: On success of above API, call FIREBASE Function for uploading file to Server'''
-
-    
     return jsonify({'status': "success", 'message': "File Uploaded Successfully"})
 
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port=int(6060),debug=True, threaded=True) 
-    #URL : localhost:6060
+    app.run(host='0.0.0.0', port=int(6060), debug=True, threaded=True)
+    # URL : localhost:6060
     # API for Uploading Files : localhost:6060/UploadFile (POST) (try POSTMAN or INSOMNIA to test POST)
